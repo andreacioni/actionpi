af6781e09953f4675049d5b0cef2c6a1be276153
Andrea Cioni <supercionci@hotmail.it>
2020-07-15 23:55:09 +0200

    WIP

--- actionpi/camera.py
+++ actionpi/camera.py
@@ -1,6 +1,7 @@
 import logging
+import re
 
-from os import path
+from os import path, listdir, lstat
 from io import BytesIO
 from abc import ABC, abstractmethod
 from threading import RLock
@@ -8,21 +9,50 @@ from threading import RLock
 
 class AbstractCamera(ABC):
 
-    def __init__(self, width: int, heigth: int, fps: int, rotation: int, output_dir: str, rolling_size: int, rolling_nums: int):
+    def __init__(self, width: int, heigth: int, fps: int, rotation: int, output_dir: str, rolling_size=0, rolling_nums=0):
         if rolling_size > 0:
             if rolling_nums < 2:
                 raise ValueError('rolling_nums must be greater then 1')
 
-        self._lock = RLock()
-        with self._lock:
-            self._width = width
-            self._heigth = heigth
-            self._fps = fps
-            self._rotation = rotation
-            self._output_dir = output_dir
-            self._output_file = path.join(output_dir, 'video.h264')
+            self._rolling_size = rolling_size
+            self._rolling_nums = rolling_nums
+            self._is_rolling_rec = True
+        else:
+            self._is_rolling_rec = False
 
-            self._camera = None
+        self._lock = RLock()
+
+        self._width = width
+        self._heigth = heigth
+        self._fps = fps
+        self._rotation = rotation
+        self._output_dir = output_dir
+
+        if self._is_rolling_rec == False:
+            self._output_file = path.join(output_dir, 'video.h264')
+        else:
+            # We increase the rolling file number to prevent accidental ovewrite on restart
+            self._current_rolling_file_number = (
+                self._evaluate_rolling_file_number(output_dir) + 1) % rolling_nums
+            self._output_file = path.join(
+                output_dir, 'video.{}.h264'.format(self._current_rolling_file_number))
+
+        self._camera = None
+
+    def _evaluate_rolling_file_number(self, output_dir: str) -> int:
+        search_regex = 'video.([1-9][0-9]*).h264'
+        video_files = [f for f in listdir(output_dir) if re.search(search_regex, f)]
+
+        if len(video_files) == 0:
+            return 1
+        
+        # Sort videos by dates
+        video_files.sort(key=lambda f: lstat(path.join(output_dir, f).st_mtime))
+
+        # Get the number of most recent video
+        match = re.search(search_regex, video_files[0])
+
+        return int(match.group(1))
 
     def get_output_dir(self) -> str:
         return self._output_dir
