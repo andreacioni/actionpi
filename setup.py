67f4daf4f0f99a4d4a6feaf138cfaf80ff853c92
Andrea Cioni <supercionci@hotmail.it>
2021-01-01 23:06:37 +0100

    Releasing v1.1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="actionpi",
-    version="1.1.4",
+    version="1.1.5",
     author="Andrea Cioni",
     description="Action/Dash camera powered by Raspberry Pi Zero",
     long_description=long_description,
